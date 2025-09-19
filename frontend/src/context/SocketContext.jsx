66e4dd78257bdeea2e4e150ae7f148ed76f450d9
Akshay Kumar <workxakshay@gmail.com>
2025-09-20 01:58:46 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mychatapp-t4fq.onrender.com/", {
+			const socket = io("https://my-chat-app-backend-ctxd.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
